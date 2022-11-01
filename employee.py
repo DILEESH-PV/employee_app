54d507e7f7107213cc4978bf00ecb4e958da79ed
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-01 17:55:40 +0530

    first commit

---
+++ employee.py
@@ -0,0 +1,29 @@
+while True:
+    print("\nSelect an option")
+    print("1 add an employee")
+    print("2 view all employee")
+    print("3 search an employee")
+    print("4 update the employee")
+    print("5 delete an employee")
+    print("6 exit")
+    
+    ch=int(input("select an option  : \n"))
+    if (ch==1):
+        print("selected add employee")
+    elif(ch==2):
+         print("selected view all employee")
+    elif(ch==3):
+        print("selected search employee")
+    elif(ch==4):
+        print("selected update the employee")
+    elif(ch==5):
+        print("selected delete employee")
+    elif(ch==6):
+        break
+        
+        
+        
+        
+    
+    
+   
